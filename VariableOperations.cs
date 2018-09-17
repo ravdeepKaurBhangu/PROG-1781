15f33d46e34f8c9d9e2667ea87dc8bef70021a98
ravdeepKaurBhangu <40557322+ravdeepKaurBhangu@users.noreply.github.com>
2018-09-17 12:41:47 +0530

    Add files via upload

---
+++ VariableOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableOperations
+{
+    class Program
+    {
+        public int x = 90;
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine(x);
+            //    Console.WriteLine("He said \"I like C# \" ");
+
+            String marks1 = "56";
+            String marks2 = "34";
+            String abc;
+            int m1,m2;
+
+            m1=int.Parse(marks1);
+            m2 = int.Parse(marks2);
+            Console.WriteLine(marks1 + marks2);
+            Console.WriteLine(m1+m2);
+
+
+           abc=m1.ToString();
+
+        }
+    }
+}
