00c7e660c0ed8e326f9d3ec59c8f62235a20bf65
ravdeepKaurBhangu <40557322+ravdeepKaurBhangu@users.noreply.github.com>
2018-11-30 10:51:10 +0530

    Add files via upload

---
+++ WritingClasses/WritingClasses/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WritingClasses
+{
+    class Class1
+    {
+        public Class1(int x)
+        {
+            MessageBox.Show("Constructor Called "+x.ToString());
+        }
+        public void display(int y)
+        {
+            MessageBox.Show("Method in a class is called");
+        }
+        public void display(double y)
+        {
+            MessageBox.Show("Method in a class is called "+y);
+        }
+    }
+}
---
+++ WritingClasses/WritingClasses/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WritingClasses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 imdw = new Class1(23);
+            imdw.display(45);
+            imdw.display(67.3);
+        }
+    }
+}
